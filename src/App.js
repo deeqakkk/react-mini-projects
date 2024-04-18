7fffe165d42222f5fa83b06129dac30889c7fdd5
Deepak Verma <0822deepakverma@gmail.com>
2024-04-18 10:34:45 +0530

    wip: react router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import UseOutsideClick from './components/custom-hooks/test2'
-function App() {
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom"
+import TabsTest from './components/custom-tabs/tabs-test'
+import StarRating from './components/star-rating/index'
+import GithubProfile from './components/github-profile/index'
+
+
+export default function App() {
   return (
-    <div className="App">
-      <UseOutsideClick />
-    </div>
+    <BrowserRouter>
+      <div className="parent-container">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <h1>This is homepage</h1>
+              <Link to='starRating'>Star rating</Link>
+              <hr></hr>
+              <Link to='tabsTest'>Custom Tabs</Link>
+              <hr></hr>
+              <Link to='githubProfile'>Github Profile</Link>
+            </>
+          } />
+          <Route path="/starRating" element={<StarRating />} />
+          <Route path="/githubProfile" element={<GithubProfile />} />
+          <Route path='/customTabs' element={<TabsTest />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
-
-export default App
